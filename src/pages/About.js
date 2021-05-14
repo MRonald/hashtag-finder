b641eb244c7c7e6592453c5979d8ca958bc9d65d
cesarsouzad <cesarsouzad@gmail.com>
2021-05-14 16:22:35 -0300

    rota about inserida

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "../styles/pages/About.module.css";
+import Header from "../components/Header";
+
+function About () {
+    return <div>
+        <Header />
+            <div>
+                Pagina sobre
+            </div>
+
+</div>
+}
+
+export default About
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import About from "./pages/About";
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/login" component={Login} />
+            <Route path="/about" component={About} />
             <Route component={() => <div>Page 404</div>} />
         </Switch>
     );
