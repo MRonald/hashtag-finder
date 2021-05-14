3738f362345698a9ab2f7eb0f56b9ea9d3737f34
João <jp.contatoprofissional@gmail.com>
2021-05-14 03:45:57 -0300

    Criando pagina de login

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "../styles/pages/Login.module.css"
+
+function Login () {
+    return <>
+    <div className={styles.bg}>
+        <div className={styles.login}>
+            <h1 className={styles.titleLogin}>Login</h1>
+            <input type="text" placeholder="Usuário"/>
+            <input type="password" placeholder="Senha"/>
+            <input className={styles.buttonLogin} type="button" value="ACESSAR"/>
+        </div>
+    </div>
+    </>
+}
+
+export default Login
