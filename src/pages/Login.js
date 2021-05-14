6de1e7fa9c9cb9c9efe8b0982db93767b2243338
João <jp.contatoprofissional@gmail.com>
2021-05-14 05:48:16 -0300

    Ajuste Background

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from "../styles/pages/Login.module.css"
-import Header from "../components/Header";
+import Header from "../components/Header"
 
 function Login () {
-    return <>
-    <Header />
-    <div className={styles.bg}>
-        <div className={styles.login}>
-            <h1 className={styles.titleLogin}>Login</h1>
-            <input type="text" placeholder="Usuário"/>
-            <input type="password" placeholder="Senha"/>
-            <input className={styles.buttonLogin} type="button" value="ACESSAR"/>
+    return <div className={styles.containerLogin}>
+        <Header />
+        <div className={styles.bgLogin}>
+            <div className={styles.login}>
+                <div className={styles.titleLogin}>Login</div>
+                <input type="text" placeholder="Usuário"/>
+                <input type="password" placeholder="Senha"/>
+                <input className={styles.buttonLogin} type="button" value="ACESSAR"/>
+            </div>
         </div>
     </div>
-    </>
 }
 
 export default Login
