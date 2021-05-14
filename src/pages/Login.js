2a7ae3865b5cb1f58d4b6da7cdc216512f4752bd
João <jp.contatoprofissional@gmail.com>
2021-05-14 03:55:43 -0300

    adicionando header a pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from "../styles/pages/Login.module.css"
+import Header from "../components/Header";
 
 function Login () {
     return <>
+    <Header />
     <div className={styles.bg}>
         <div className={styles.login}>
             <h1 className={styles.titleLogin}>Login</h1>
