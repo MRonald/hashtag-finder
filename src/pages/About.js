adbe58ee27799ab73a3ce773bc0af109eb0e8e4a
cesarsouzad <cesarsouzad@gmail.com>
2021-05-15 02:11:04 -0300

    correção background, atualização css parte sobre

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from "../styles/pages/About.module.css";
 import Header from "../components/Header";
+import ilustração from './../assets/img/about-ilustration.svg'
 
 function About () {
-    return <div>
+    return <div className={styles.bgabout}>
         <Header />
-            <div>
-            <h1>Sobre o Projeto</h1>
-                    <p>
+             <div className={styles.firstContent}> {/*primeiro conteudo */}
+                <div className={styles.contentAbout}>
+                    <h1 className={styles.h1about}>Sobre o Projeto</h1>
+                    <p className={styles.pabout}>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sedundefined
                     </p>
+                </div>
+                <div>
+                <img className={styles.ilustration} src={ilustração} alt="Ilustração" />
+                </div>
             </div>
 
 </div>
