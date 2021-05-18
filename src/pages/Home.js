58c0c1c7bf151f9bbc01b07930ca78e90a650e51
João <jp.contatoprofissional@gmail.com>
2021-05-18 01:13:59 -0300

    ajustes branch cesar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Header from "../components/Header";
 
@@ -35,39 +34,3 @@ export default function Home() {
         </div>
     );
 }
-=======
-import React from 'react';
-import Header from "../components/Header";
-
-import styles from '../styles/pages/Home.module.css';
-
-// Images
-import iconSearch from '../assets/img/icon-search.svg';
-
-export default function Home() {
-    return (
-        <div>
-            <div className={styles.hero}>
-                <Header />
-                <div className={styles.heroContent}>
-                    <div>
-                        <h1>Encontre hashtags de maneira fácil.</h1>
-                        <h2>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo</h2>
-                    </div>
-                    <form>
-                        <label htmlFor="inputMain">
-                            <img src={iconSearch} alt="" />
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Buscar..."
-                            className={styles.inputMain}
-                            id="inputMain"
-                        />
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
-}
->>>>>>> cesar
