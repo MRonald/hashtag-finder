bab25deee487f157a5457a83c6c0de180e124853
MRonald <ronaldmichael918@gmail.com>
2021-05-19 15:54:00 -0300

    Atualizando correção de warnings na página About

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,40 +32,40 @@ function About() {
         </div>
 
         <h1 className={styles.who}>Quem somos</h1>
-        <div class={styles.wrapperflex}>
+        <div className={styles.wrapperflex}>
         {/* USUARIOS */}
-            <div class={styles.container}>
-                <div class={styles.bannerimg}></div>
-                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' class={styles.profileimg}/>
-                <h1 class={styles.name}>Nome Sobrenome</h1>
-                <p class={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
+            <div className={styles.container}>
+                <div className={styles.bannerimg}></div>
+                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' className={styles.profileimg}/>
+                <h1 className={styles.name}>Nome Sobrenome</h1>
+                <p className={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
                 <img className={styles.github} src={github} alt="" />
                 <img className={styles.email} src={email} alt="" />
                 <img className={styles.linkedin} src={linkedin} alt="" />
             </div>
-            <div class={styles.container}>
-                <div class={styles.bannerimg}></div>
-                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' class={styles.profileimg}/>
-                <h1 class={styles.name}>Nome Sobrenome</h1>
-                <p class={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
+            <div className={styles.container}>
+                <div className={styles.bannerimg}></div>
+                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' className={styles.profileimg}/>
+                <h1 className={styles.name}>Nome Sobrenome</h1>
+                <p className={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
                 <img className={styles.github} src={github} alt=""/>
                 <img className={styles.email} src={email} alt=""/>
                 <img className={styles.linkedin} src={linkedin} alt=""/>
             </div>
-            <div class={styles.container}>
-                <div class={styles.bannerimg}></div>
-                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' class={styles.profileimg}/>
-                <h1 class={styles.name}>Nome Sobrenome</h1>
-                <p class={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
+            <div className={styles.container}>
+                <div className={styles.bannerimg}></div>
+                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' className={styles.profileimg}/>
+                <h1 className={styles.name}>Nome Sobrenome</h1>
+                <p className={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
                 <img className={styles.github} src={github} alt=""/>
                 <img className={styles.email} src={email} alt=""/>
                 <img className={styles.linkedin} src={linkedin} alt=""/>
             </div>
-            <div class={styles.container}>
-                <div class={styles.bannerimg}></div>
-                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' class={styles.profileimg}/>
-                <h1 class={styles.name}>Nome Sobrenome</h1>
-                <p class={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
+            <div className={styles.container}>
+                <div className={styles.bannerimg}></div>
+                <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/99b20742-4dba-46de-864c-4e2fe5fb3f4f/d4hhdbc-10e39559-98ac-42c4-a4b2-7f08c1b799f7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk5YjIwNzQyLTRkYmEtNDZkZS04NjRjLTRlMmZlNWZiM2Y0ZlwvZDRoaGRiYy0xMGUzOTU1OS05OGFjLTQyYzQtYTRiMi03ZjA4YzFiNzk5ZjcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X_VZom3qdcKcETuLbIevHqF2wEpo21tYmHgfA57Xtdk' alt='profile' className={styles.profileimg}/>
+                <h1 className={styles.name}>Nome Sobrenome</h1>
+                <p className={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat...</p>
                 <img className={styles.github} src={github} alt=""/>
                 <img className={styles.email} src={email} alt=""/>
                 <img className={styles.linkedin} src={linkedin} alt=""/>
