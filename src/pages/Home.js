3a98b44a37d2776ae13943c2ebe610f81a99d945
MRonald <ronaldmichael918@gmail.com>
2021-05-26 18:52:13 -0300

    Inserindo busca hashtag no airtable | Adicionando config.js | Adicionando axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,77 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import styles from '../styles/pages/Home.module.css';
 
-import Header from "../components/Header";
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ImageResult from '../components/ImageResult';
 import TextResult from '../components/TextResult';
+import { BASE_URL_SEARCH } from '../config';
 
 // Images
 import iconSearch from '../assets/img/icon-search.svg';
 
 export default function Home() {
     const [buttonActive, setButtonActive] = useState("images");
+    const [textSearch, setTextSearch] = useState("");
+
+    function getCurrentDate() {
+        const date = new Date();
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+
+    function getCurrentHour() {
+        const date = new Date();
+        const hour = String(date.getHours()).padStart(2, '0');
+        const minute = String(date.getMinutes()).padStart(2, '0');
+        return `${hour}:${minute}`;
+    }
+
+    function handleTextChange(event) {
+        setTextSearch(event.target.value);
+    }
+
+    function validateForm() {
+        if (textSearch.length < 1) {
+            return false;
+        }
+        return true;
+    }
+
+    function submitForm(event) {
+        event.preventDefault();
+        const isValid = validateForm();
+        if (!isValid) {
+            alert('preencha o campo')
+        } else {
+            registerSearch();
+        }
+        setTextSearch("");
+    }
+
+    function registerSearch() {
+        axios.post(BASE_URL_SEARCH, {
+            "records": [
+                {
+                    "fields": {
+                        "Squad": "2",
+                        "Hashtag": textSearch,
+                        "Data": getCurrentDate(),
+                        "Hora": getCurrentHour()
+                    }
+                }
+            ]
+        }, {
+            headers: {
+                "Authorization": "Bearer key2CwkHb0CKumjuM",
+                "Content-Type": "application/json"
+            }
+        }).catch(e => console.log('erro\n' + e));
+    }
 
     return (
         <div>
@@ -22,7 +82,7 @@ export default function Home() {
                         <h1>Encontre hashtags de maneira fácil.</h1>
                         <h2>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo</h2>
                     </div>
-                    <form>
+                    <form onSubmit={submitForm}>
                         <button>
                             <img src={iconSearch} alt="" />
                         </button>
@@ -30,6 +90,8 @@ export default function Home() {
                             type="text"
                             placeholder="Buscar..."
                             className={styles.inputMain}
+                            value={textSearch}
+                            onChange={handleTextChange}
                         />
                     </form>
                 </div>
