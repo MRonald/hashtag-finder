ab7f7f029ca031c0860d0f61a246d15d84d8c54d
MRonald <ronaldmichael918@gmail.com>
2021-05-28 20:55:22 -0300

    Inserindo zoom na imagem miniatura

--- src/components/ImageResult.js
+++ src/components/ImageResult.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../styles/components/ImageResult.module.css';
 
 export default function ImageResult({ result }) {
+    const [className, setClassName] = useState(`${styles.imageResultWrapper}`)
+
+    function toggleClassName() {
+        if (className.includes('zoomView')) {
+            setClassName(`${styles.imageResultWrapper}`)
+        } else {
+            setClassName(`${styles.imageResultWrapper} ${styles.zoomView}`)
+        }
+    }
+
     return (
         <div
-            className={styles.imageResultWrapper}
-            style={{ backgroundImage: `url(${result.image_url})` }}
+            className={className}
+            style={{
+                backgroundImage: `url(${result.image_url})`
+            }}
+            onClick={toggleClassName}
+            onBlur={() => alert()}
         >
             <div className={styles.infoPost}>
                 <span>Postado por:</span>
