958b164737cefe485e2016dc31b976bda2765113
MRonald <ronaldmichael918@gmail.com>
2021-05-31 15:21:18 -0300

    Validando exibição da lista de buscas realizadas

--- src/pages/Historic.js
+++ src/pages/Historic.js
@@ -6,6 +6,7 @@ import styles from "../styles/pages/Historic.module.css"
 
 export default function Historic() {
     const [searchItems, setSearchItems] = useState([]);
+    const [logged, setLogged] = useState(false);
 
     function getURL() {
         return `https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas?filterByFormula=({Squad}='2')&_Limit='5'`;
@@ -29,7 +30,10 @@ export default function Historic() {
                 )
                 setSearchItems(results);
             }
-        )
+        );
+        if (sessionStorage.getItem('login') === 'true') {
+            setLogged(true);
+        }
     }, []);
 
     return (
@@ -38,18 +42,25 @@ export default function Historic() {
             <div className={styles.container}>
                 <h2>Buscas realizadas</h2>
                 <div className={styles.list}>
-                    {/* Topo da lista */}
-                    <div className={styles.listTop}>
-                        <div className={styles.listHeader}>
-                            <div className={styles.hashtag}>Hashtag</div>
-                            <div className={styles.date}>Data</div>
-                            <div className={styles.time}>Hora</div>
-                        </div>
-                    </div>
-                    {
-                        /* Dados das buscas realizadas */
-                        searchItems.map(item => <SearchItem item={item} />)
-                    }
+                    {logged ? (
+                        <>
+                            <div className={styles.listTop}>
+                                <div className={styles.listHeader}>
+                                    <div className={styles.hashtag}>Hashtag</div>
+                                    <div className={styles.date}>Data</div>
+                                    <div className={styles.time}>Hora</div>
+                                </div>
+                            </div>
+                            <>
+                                {
+                                    /* Dados das buscas realizadas */
+                                    searchItems.map(item => <SearchItem item={item} />)
+                                }
+                            </>
+                        </>
+                    ) : (
+                        <p className={styles.accessDenied}>Você não tem permissão para acessar essa página</p>
+                    )}
                 </div>
             </div>
         </div>
