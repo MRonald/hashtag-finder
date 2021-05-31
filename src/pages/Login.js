a6779bfd543440e0b92526f13cfbc7ece67b3a8a
MRonald <ronaldmichael918@gmail.com>
2021-05-31 16:43:20 -0300

    Adicionando mensagem de login inválido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,54 +1,59 @@
-import React, { useState } from 'react'
-import styles from "../styles/pages/Login.module.css"
-import Header from "../components/Header"
-import axios from 'axios'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import axios from 'axios';
 import { Redirect } from 'react-router';
+
+import styles from '../styles/pages/Login.module.css';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [logged, setLogged] = useState(false);
+    const [invalid, setInvalid] = useState(false);
 
     function getURL(email, pass) {
         return `https://api.airtable.com/v0/app6wQWfM6eJngkD4/Login?api_key=key2CwkHb0CKumjuM&filterByFormula=(AND({Email}="${email}",{Senha}="${pass}",{Squad}="2"))`;
     }
 
     function handleEmail(event) {
-        setEmail(event.target.value)
+        setEmail(event.target.value);
     }
 
     function handlePassword(event) {
-        setPassword(event.target.value)
+        setPassword(event.target.value);
     }
 
     function validateEmail() {
-        if (email === "") {
-            return false
+        if (email === '') {
+            return false;
         } else {
-            return true
+            return true;
         }
     }
 
     function validatePassword() {
-        if (password === "") {
-            return false
+        if (password === '') {
+            return false;
         } else {
-            return true
+            return true;
         }
     }
 
     function handleSubmit(event) {
-        event.preventDefault(event)
-        const emailValid = validateEmail()
-        const passValid = validatePassword()
+        event.preventDefault(event);
+        setInvalid(false);
+        const emailValid = validateEmail();
+        const passValid = validatePassword();
         if (emailValid === true && passValid === true) {
             axios.get(getURL(email, password)).then(
                 response => {
                     if (response.data.records.length > 0) {
                         sessionStorage.setItem('login', 'true');
+                        setInvalid(false);
                         setLogged(true);
                     } else {
                         sessionStorage.setItem('login', 'false');
+                        setInvalid(true);
                         setLogged(false);
                     }
                 }
@@ -68,7 +73,7 @@ function Login() {
                         type="email"
                         placeholder="Usuário"
                         id="inpEmail"
-                        autoComplete='off'
+                        autoComplete="off"
                         onChange={handleEmail}
                         required
                     />
@@ -85,6 +90,11 @@ function Login() {
                         value="ACESSAR"
                     />
                     {logged && <Redirect to="/historic" />}
+                    {invalid && (
+                        <span className={styles.accessDenied}>
+                            Usuário e/ou senha inválidos
+                        </span>
+                    )}
                 </form>
             </div>
         </div>
