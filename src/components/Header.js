efb6bee54152f3047cb6a4ade3be62fffd36cc5d
MRonald <ronaldmichael918@gmail.com>
2021-05-31 15:30:57 -0300

    Redirecionando usuário logado automaticamente

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from '../styles/components/Header.module.css';
 
 // Images
@@ -7,10 +9,17 @@ import infoCircle from '../assets/img/icon-info-circle.svg';
 import userLogin from '../assets/img/icon-user-alt.svg';
 import homeIcon from '../assets/img/icon-home.svg';
 import logoutIcon from '../assets/img/icon-power-off.svg';
-import { Link } from 'react-router-dom';
 
 
 export default function Header({ fixed, template }) {
+    const [logged, setLogged] = useState(false);
+
+    useEffect(() => {
+        if (sessionStorage.getItem('login') === 'true') {
+            setLogged(true);
+        }
+    }, []);
+
     function setLoggedStatus() {
         sessionStorage.setItem('login', 'false');
     }
@@ -18,10 +27,10 @@ export default function Header({ fixed, template }) {
     return (
         <div
             className={styles.headerWrapper}
-            style={{position: `${fixed === 'true' ? 'fixed' : 'initial'}`}}
+            style={{ position: `${fixed === 'true' ? 'fixed' : 'initial'}` }}
         >
             <Link to="/" className={styles.linkImageHome}>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" />
             </Link>
             {/*
                 Existem 3 templates para os botões:
@@ -32,19 +41,27 @@ export default function Header({ fixed, template }) {
             {template === 'home' && (
                 <div className={styles.linksWrapper}>
                     <Link to="/about" className={styles.firstButton}>
-                        <img src={infoCircle} alt=""/>
+                        <img src={infoCircle} alt="" />
                         Sobre
                     </Link>
-                    <Link to="/login" className={styles.secondButton}>
-                        <img src={userLogin} alt=""/>
-                        Login
-                    </Link>
+                    {logged ? (
+                        <Link to="/historic" className={styles.secondButton}>
+                            <img src={userLogin} alt="" />
+                            Login
+                        </Link>
+                    ) : (
+                        <Link to="/login" className={styles.secondButton}>
+                            <img src={userLogin} alt="" />
+                            Login
+                        </Link>
+                    )}
+
                 </div>
             )}
             {template === 'login' && (
                 <div className={styles.linkWrapper}>
                     <Link to="/" className={styles.firstButton}>
-                        <img src={homeIcon} alt=""/>
+                        <img src={homeIcon} alt="" />
                         Home
                     </Link>
                 </div>
@@ -52,11 +69,11 @@ export default function Header({ fixed, template }) {
             {template === 'tableSearch' && (
                 <div className={styles.linksWrapper}>
                     <Link to="/" className={styles.firstButton}>
-                        <img src={homeIcon} alt=""/>
+                        <img src={homeIcon} alt="" />
                         Home
                     </Link>
                     <Link to="/" className={styles.secondButton} onClick={setLoggedStatus}>
-                        <img src={logoutIcon} alt=""/>
+                        <img src={logoutIcon} alt="" />
                         Sair
                     </Link>
                 </div>
